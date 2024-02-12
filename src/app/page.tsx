419e4603696f5d0781cea807856532b2786065ae
grestavia <blahsheepuwu@gmail.com>
2024-02-12 11:15:22 +0700

    add: search system fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,15 @@ import Sidebar from "./components/sidebar";
 import Link from "next/link";
 import SearchIcon from "@mui/icons-material/Search";
 import axios from "axios";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
-  const [sidebar, setSidebar] = useState(true);
+  const [search, setSearch] = useState("");
   const [imagespath, setImagesPath] = useState<any[]>([]);
+  const [imagesearchpath, setImagesSearchPath] = useState<any[]>([]);
+  const [searchpage, setSearchPage] = useState(false);
 
-  useEffect(()=> {
+  useEffect(() => {
     const fetchImage = async () => {
       const response = await axios.get(process.env.NEXT_PUBLIC_API_RATIO + "/photos", {
         headers: {
@@ -18,49 +21,123 @@ export default function Home() {
       });
       const imageData = response.data.data;
       if (Array.isArray(imageData)) {
-        const imageIds = imageData.map(image => image.id);
         setImagesPath(imageData);
       }
     }
 
     fetchImage();
-  },[])
+  }, [])
+
+  const handleSearchSubmit = async (e: any) => {
+    e.preventDefault();
+    const token = localStorage.getItem("token");
+    try {
+      const response = await axios.get(process.env.NEXT_PUBLIC_API_RATIO + `/photos?title=${search}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        }
+      });
+      const imageSearchData = response.data.data;
+      setSearchPage(true);
+      console.log(imageSearchData);
+      if (Array.isArray(imageSearchData)) {
+        setImagesSearchPath(imageSearchData);
+      }
+    } catch (error) {
+
+    }
+  }
+  const handleSearchChange = (e: any) => {
+    setSearch(e.target.value);
+  }
 
   return (
     <>
       <div className="flex justify-between pt-20 px-5 lg:pr-10 lg:pl-0">
-        {sidebar && <Sidebar />}
-        <div className="konten flex justify-between scrollbar-thin scrollbar-thumb-neutral-300 flex-col w-full px-5 pt-5 bg-white h-[calc(100vh-110px)] rounded-lg">
-          <div className="wrap overflow-scroll scrollbar-thin scrollbar-thumb-neutral-300 w-full overflow-x-hidden">
-            <div className="lg:columns-4 md:columns-3 columns-2 gap-3">
-              { imagespath.map((image, index) => {
-                return (
-                  <Link href={`/post/${image.id}`} key={index}>
-                    <div className="mb-3 hover:brightness-[.80] rounded-md transform hover:scale-[102%] transition ease-in">
-                      <img
-                        src={process.env.NEXT_PUBLIC_API_RATIO + `/files/images/photos/${image.locationFile}`}
-                        className="rounded-md mb-2"
-                        alt=""
-                      />
-                    </div>
-                  </Link>
-                );
-              }) }
+        <Sidebar />
+        {searchpage ? (
+          <>
+            <div className="konten flex justify-between scrollbar-thin scrollbar-thumb-neutral-300 flex-col w-full px-5 pt-5 bg-white h-[calc(100vh-110px)] rounded-lg">
+            <div className="wrap overflow-scroll scrollbar-thin scrollbar-thumb-neutral-300 w-full overflow-x-hidden">
+                { imagesearchpath.length === 0 ? (
+                  <>
+                 <p className="text-center">Tidak Ada Postingan Terkait Dengan Kata Kunci "<b>{search}</b>"</p>
+                  </>
+                ) : (
+                  <>
+                  <div className="lg:columns-4 md:columns-3 columns-2 gap-3">
+                  {imagesearchpath.map((image, index) => {
+                    return (
+                      <Link href={`/post/${image.id}`} key={index}>
+                        <div className="mb-3 hover:brightness-[.80] rounded-md transform hover:scale-[102%] transition ease-in">
+                          <img
+                            src={process.env.NEXT_PUBLIC_API_RATIO + `/files/images/photos/${image.locationFile}`}
+                            className="rounded-md mb-2"
+                            alt=""
+                          />
+                        </div>
+                      </Link>
+                    );
+                  })}
+                </div>
+                  </>
+                ) }
+              </div>
+              <form onSubmit={handleSearchSubmit} action="">
+                <div className="searchbar bg-white p-2 w-full">
+                  <div className="w-full flex justify-between rounded-xl border-2 p-1 border-[#07A081]">
+                    <input
+                      type="text"
+                      onChange={handleSearchChange}
+                      className="w-full pl-5 focus:outline-none"
+                      placeholder="Temukan Gambar.."
+                    />
+                    <Button type="submit" className="bg-[#07A081] text-white p-2 rounded-lg">
+                      <SearchIcon />
+                    </Button>
+                  </div>
+                </div>
+              </form>
             </div>
-          </div>
-          <div className="searchbar bg-white p-2 w-full">
-            <div className="w-full flex justify-between rounded-xl border-2 p-1 border-[#07A081]">
-              <input
-                type="text"
-                className="w-full pl-5 focus:outline-none"
-                placeholder="Temukan Gambar.."
-              />
-              <button className="bg-[#07A081] text-white p-2 rounded-lg">
-                <SearchIcon />
-              </button>
+          </>
+        ) : (
+          <>
+            <div className="konten flex justify-between scrollbar-thin scrollbar-thumb-neutral-300 flex-col w-full px-5 pt-5 bg-white h-[calc(100vh-110px)] rounded-lg">
+              <div className="wrap overflow-scroll scrollbar-thin scrollbar-thumb-neutral-300 w-full overflow-x-hidden">
+                <div className="lg:columns-4 md:columns-3 columns-2 gap-3">
+                  {imagespath.map((image, index) => {
+                    return (
+                      <Link href={`/post/${image.id}`} key={index}>
+                        <div className="mb-3 hover:brightness-[.80] rounded-md transform hover:scale-[102%] transition ease-in">
+                          <img
+                            src={process.env.NEXT_PUBLIC_API_RATIO + `/files/images/photos/${image.locationFile}`}
+                            className="rounded-md mb-2"
+                            alt=""
+                          />
+                        </div>
+                      </Link>
+                    );
+                  })}
+                </div>
+              </div>
+              <form onSubmit={handleSearchSubmit} action="">
+                <div className="searchbar bg-white p-2 w-full">
+                  <div className="w-full flex justify-between rounded-xl border-2 p-1 border-[#07A081]">
+                    <input
+                      type="text"
+                      onChange={handleSearchChange}
+                      className="w-full pl-5 focus:outline-none"
+                      placeholder="Temukan Gambar.."
+                    />
+                    <Button type="submit" className="bg-[#07A081] text-white p-2 rounded-lg">
+                      <SearchIcon />
+                    </Button>
+                  </div>
+                </div>
+              </form>
             </div>
-          </div>
-        </div>
+          </>
+        )}
       </div>
     </>
   );
