bbbc8c4ec695c2d24a50174e7f7110f0f693577a
grestavia <blahsheepuwu@gmail.com>
2024-02-29 07:34:19 +0700

    add: delete follower

--- src/app/components/profile/follower.tsx
+++ src/app/components/profile/follower.tsx
@@ -1,7 +1,27 @@
+'use client'
 import { Button, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, User } from "@nextui-org/react";
 import Link from "next/link";
+import axios from "axios";
+import { useState } from "react";
 
-export default function Follower({isOpen, onClose, followers}: any) {
+export default function Follower({ isOpen, onClose, followers, owner }: any) {
+
+    const [deletedFollowers, setDeletedFollowers] = useState<string[]>([]);
+
+    const handleremovefollower = async (followerId: string, index: any) => {
+        const token = localStorage.getItem("token");
+        try {
+            const response = await axios.delete(process.env.NEXT_PUBLIC_API_RATIO + `/users/followers/${followerId}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            })
+            console.log(response.data);
+            setDeletedFollowers([...deletedFollowers, followerId]);
+        } catch (error) {
+            console.error("Failed :", error);
+        }
+    }
     return (
         <>
             <Modal
@@ -17,20 +37,32 @@ export default function Follower({isOpen, onClose, followers}: any) {
                         </ModalHeader>
                         <ModalBody>
                             {followers.map((follower: any, index: any) => (
-                                <div key={index}>
-                                    <Link key={index} href={`/profile/${follower.username}`}>
-                                        <Button className="py-7 px-2 bg-transparent flex justify-start">
-                                            <User
-                                                name={follower.fullName}
-                                                description={<p>@{follower.username}</p>}
-                                                avatarProps={{
-                                                    src: follower.photoUrl && process.env.NEXT_PUBLIC_API_RATIO + `/files/images/profiles/${follower.photoUrl}`,
-                                                }}
-                                            />
-                                        </Button>
-                                    </Link>
+                                <>
+                                    <div key={index} className="flex flex-row items-center justify-between w-full">
+                                        <Link key={index} href={`/profile/${follower.username}`}>
+                                            <Button className="py-7 px-2 bg-transparent">
+                                                <User
+                                                    name={follower.fullName}
+                                                    description={<p>@{follower.username}</p>}
+                                                    avatarProps={{
+                                                        src: follower.photoUrl && process.env.NEXT_PUBLIC_API_RATIO + `/files/images/profiles/${follower.photoUrl}`,
+                                                    }}
+                                                />
+                                            </Button>
+                                        </Link>
+                                        {owner ? (
+                                            <>
+                                                {!deletedFollowers.includes(follower.followerId) ? (
+                                                    <Button color="danger" onClick={() => handleremovefollower(follower.followerId, index)}>Hapus</Button>
+                                                ) : (
+                                                    // Tombol disabled dan teks Terhapus jika follower sudah dihapus
+                                                    <Button isDisabled color="default">Terhapus</Button>
+                                                )}
+                                            </>
+                                        ) : (<></>)}
+                                    </div>
                                     <hr />
-                                </div>
+                                </>
                             ))}
                         </ModalBody>
                         <ModalFooter>
